64999ef06b01ce840d09dff71997287c806441a6
Zach Lasky <zacharylasky123@gmail.com>
2019-07-30 12:36:24 -0400

    added some styling to sign-in

--- trip-split/src/Components/Login/LoginForm.js
+++ trip-split/src/Components/Login/LoginForm.js
@@ -1,16 +1,24 @@
-import React from 'react';
+import React, { useState } from 'react';
+import styled from 'styled-components';
+//STYLE
+const LoginDiv = styled.div`width: 500px; border: 1px solid red; text-align: center; height: 200px;`;
+const InputDiv = styled.div`display: flex; justify-content: center;`;
+const Input = styled.input`margin-left: 10px; border-radius: 3px;`;
+const Button = styled.button`margin-top: 20px; border-radius: 10px; width: 100px; height: 40px;`;
 
 function LoginForm() {
 
   return (
-    <div className="login-container">
-      <form onSubmit={null}>
-        <label>Email
-          <input value={null} name="name" onChange={null} type="email" />
-        </label>
-          <button type="submit">Login</button>
-      </form>
-    </div>
+    <LoginDiv className="login-container">
+      <h1>Sign In</h1>
+        <form onSubmit={null}>
+          <InputDiv>
+            <Input placeholder="Email" value={null} name="name" onChange={null} type="email" />
+            <Input placeholder="Password" value={null} name="name" onChange={null} type="password" />
+          </InputDiv>
+            <Button type="submit">Sign In</Button>
+        </form>
+    </LoginDiv>
   )
 }
 
