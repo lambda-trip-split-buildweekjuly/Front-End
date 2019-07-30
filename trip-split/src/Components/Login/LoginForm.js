acf4f8fc5424e4f9c72d8316b072cadb37e8f0bc
Zach Lasky <zacharylasky123@gmail.com>
2019-07-30 18:36:49 -0400

    functioning login and register

--- trip-split/src/Components/Login/LoginForm.js
+++ trip-split/src/Components/Login/LoginForm.js
@@ -1,22 +1,49 @@
 import React, { useState } from 'react';
 import styled from 'styled-components';
 import logo from '../../Images/logo.png';
+import { connect } from 'react-redux';
+import { register, login, getHome, getUsers, getUser, updateUser, deleteUser } from '../../actions/actions';
 //STYLE
 const LoginDiv = styled.div`border-bottom: 1px solid black; text-align: center; height: 560px;`;
 const InputDiv = styled.div`display: flex; justify-content: center;`;
 const Input = styled.input`margin-left: 10px; border-radius: 3px;`;
 const Button = styled.button`margin-top: 20px; border-radius: 10px; width: 100px; height: 40px;`;
 
-function LoginForm() {
+function LoginForm(props) {
+
+  const [credentials, setCredentials] = useState({
+    email: "",
+    password: ""
+  })
+
+  function onChange(event) {
+    setCredentials({ ...credentials, [event.target.name]: event.target.value });
+    console.log(credentials);
+  }
+
+  function onSubmit(event) {
+    event.preventDefault();
+    loginHandler();
+    console.log(credentials);
+    setCredentials({email: "", password: ""});
+  }
+
+  const loginHandler = () => {
+    let credss = {
+      email: credentials.email,
+      password: credentials.password
+    }
+    props.login(credss)
+  }
 
   return (
     <LoginDiv className="login-container">
       <img src={logo}></img>
       <h1>Sign In</h1>
-        <form onSubmit={null}>
+        <form onSubmit={onSubmit}>
           <InputDiv>
-            <Input placeholder="Email" value={null} name="name" onChange={null} type="email" />
-            <Input placeholder="Password" value={null} name="name" onChange={null} type="password" />
+            <Input placeholder="Email" value={credentials.email} name="email" onChange={onChange} type="email" />
+            <Input placeholder="Password" value={credentials.password} name="password" onChange={onChange} type="password" />
           </InputDiv>
             <Button type="submit">Sign In</Button>
         </form>
@@ -24,4 +51,9 @@ function LoginForm() {
   )
 }
 
-export default LoginForm;
+function mapStateToProps(state){
+  // console.log(state)
+  return {}
+}
+
+export default connect(mapStateToProps, {register, login, getHome, getUsers, getUser, updateUser, deleteUser})(LoginForm);
