c1d427ea034be26887ed69e70e735054a9efa6d0
TheHaunted <kevin_lopez@live.com>
2019-07-30 11:30:00 -0500

    made inputs for form

---
+++ trip-split/src/components/PeopleForm.js
@@ -0,0 +1,34 @@
+import React from 'react';
+
+export default function PeopleForm(props){
+    return (
+        <div>
+            <form>
+                <input
+                    placeholder="Name"
+                    name="name"
+                    value={personalbar.name}
+                    onChange={handleChange}
+                />
+                <input
+                    placeholder="Name"
+                    name="name"
+                    value={personalbar.name}
+                    onChange={handleChange}
+                />
+                <input
+                    placeholder="Name"
+                    name="name"
+                    value={personalbar.name}
+                    onChange={handleChange}
+                />
+                <input
+                    placeholder="Name"
+                    name="name"
+                    value={personalbar.name}
+                    onChange={handleChange}
+                />
+            </form>
+        </div>
+    )
+}
--- trip-split/src/components/PeopleList.js
+++ trip-split/src/components/PeopleList.js
@@ -1,9 +1,11 @@
 import React from 'react';
+import PeopleForm from './PeopleForm.js';
 
 export default function PeopleList(props){
     return (
         <div>
             <h1>Who's going</h1>
+            <PeopleForm />
         </div>
     )
 }
