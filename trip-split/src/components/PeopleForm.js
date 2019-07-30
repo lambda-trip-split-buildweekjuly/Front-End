c2952accbfc56562e5be63a7b8a2dd678eed38c8
TheHaunted <kevin_lopez@live.com>
2019-07-30 16:16:36 -0500

    finished PeopleForm

--- trip-split/src/components/PeopleForm.js
+++ trip-split/src/components/PeopleForm.js
@@ -1,33 +1,54 @@
-import React from 'react';
+import React, {useState} from 'react';
 
 export default function PeopleForm(props){
+    const [people, setPeople] = useState({trip: '', person1: '', person2: '', person3:'', person4: '', person5: ''});
+    const handleChange = event => {
+        setPeople({...people, [event.target.name]: event.target.value});
+    };
+    const handleSubmit = event => {
+        event.preventDefault();
+        console.log(people);
+    }
     return (
         <div>
-            <form>
+            <form className="trip-form" onSubmit={handleSubmit}>
+                <input
+                placeholder="Trip Name"
+                name="trip"
+                value={people.trip}
+                onChange={handleChange}
+                />
                 <input
                     placeholder="Name"
-                    name="name"
-                    value={personalbar.name}
+                    name="person1"
+                    value={people.person1}
                     onChange={handleChange}
                 />
                 <input
                     placeholder="Name"
-                    name="name"
-                    value={personalbar.name}
+                    name="person2"
+                    value={people.person2}
                     onChange={handleChange}
                 />
                 <input
                     placeholder="Name"
-                    name="name"
-                    value={personalbar.name}
+                    name="person3"
+                    value={people.person3}
                     onChange={handleChange}
                 />
                 <input
                     placeholder="Name"
-                    name="name"
-                    value={personalbar.name}
+                    name="person4"
+                    value={people.person4}
                     onChange={handleChange}
                 />
+                <input
+                    placeholder="Name"
+                    name="person5"
+                    value={people.person5}
+                    onChange={handleChange}
+                />
+                <button>Submit</button>
             </form>
         </div>
     )
