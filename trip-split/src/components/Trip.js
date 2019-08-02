892277c2ad862c81d27cde1e9adcda07758c1861
Zach Lasky <zacharylasky123@gmail.com>
2019-08-01 20:53:07 -0400

    trip data created

--- trip-split/src/components/Trip.js
+++ trip-split/src/components/Trip.js
@@ -1,24 +1,38 @@
 import React, { useEffect, useState } from 'react';
 import ExpenseForm from './ExpenseForm';
+import { connect } from 'react-redux';
+import { getTripsByUserId } from '../actions/actions';
 //STYLE
 
 function Trip(props) {
   const [formToggle, setFormToggle] = useState(false);
   console.log("trip-props", props)
+  const [trip, setTrip] = useState({
 
+  })
   useEffect(() => {
     function getTripData() {
       const id = props.match.params.id;
-      const tripObj = props.location.state;
-      console.log("id", id);
+      const tripObj = props.location.state.trip;
+      // console.log("id", id);
+      setTrip(tripObj);
+      
     }
     getTripData()}, [props.match.params.id, props.location.state])
 
+  useEffect(() => {
+    console.log("Please dont be an infinite loop")
+    const user_id = localStorage.getItem('user_id')
+    props.getTripsByUserId(user_id)
+  }, [props.getTripsTrigger])
+
+
+
   return (
       formToggle 
-        ? <ExpenseForm setFormToggle={setFormToggle}/> 
+        ? <ExpenseForm setFormToggle={setFormToggle} trip={trip}/> 
         :<div className="trip-container">
-          <h1>Trip</h1>
+          <h1>{trip.trip_name}</h1>
           <div className="expense-section">
             <button onClick={() => setFormToggle(true)}><h2>+</h2></button>
             <h1>New Expense</h1>
@@ -36,4 +50,10 @@ function Trip(props) {
   )
 }
 
-export default Trip;
+function mapStateToProps(state){
+  return {
+    getTripsTrigger: state.getTripsTrigger
+  }
+}
+
+export default connect(mapStateToProps, {getTripsByUserId})(Trip);
